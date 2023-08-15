03ebeb58a868d4c5072272a342208d29a9eae0f2
narendra <51484021+alenway@users.noreply.github.com>
2023-08-15 22:31:06 +0530

    connected to mongo db

---
+++ src/app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+    return(
+        <div className="flex">
+        <h1 className="text-white text-center text-2xl">Login</h1>
+        </div>
+    )
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/navigation";
+import { axios } from "axios";
+export default function Signup() {
+    return(
+        <div className="flex">
+            <h1 className="text-center text-white text-2xl">Signup</h1>
+        </div>
+    )
+}
