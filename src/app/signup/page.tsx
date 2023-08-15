11a0be2dffcfcd9516d67e4a3c23ed1d7b6c19dd
narendra <51484021+alenway@users.noreply.github.com>
2023-08-15 23:00:54 +0530

    signup page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -3,10 +3,34 @@ import Link from "next/link";
 import React from "react";
 import { useRouter } from "next/navigation";
 import { axios } from "axios";
+
 export default function Signup() {
-    return(
-        <div className="flex">
+    const [user, setUser] = React.useState({
+        email: "",
+        password: "",
+        username: "",
+    });
+
+    const onSignup = async () => {
+
+    }
+
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1 className="text-center text-white text-2xl">Signup</h1>
+            <hr />
+            <label htmlFor="username">username</label>
+            <input className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600" type="text" value={user.username} id="username" onChange={(e) => setUser({ ...user, username: e.target.value })} placeholder="username" />
+
+            <label htmlFor="email">email</label>
+            <input className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600" type="text" value={user.email} id="email" onChange={(e) => setUser({ ...user, email: e.target.value })} placeholder="email" />
+
+            <label htmlFor="password">password</label>
+            <input className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600" type="password" value={user.password} id="password" onChange={(e) => setUser({ ...user, password: e.target.value })} placeholder="password" />
+
+            <button onClick={onSignup} className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">Signup here</button>
+
+            <Link href="/login">visit login page</Link>
         </div>
     )
 }
